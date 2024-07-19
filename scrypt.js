91a30d5bffbde902a26561776eaec3fc4ebe9774
Leonan Cardoso <leonangomes1311@hotmail.com>
2024-07-19 00:29:42 -0300

    Adicionado a função para o botao de copiar texto

--- scrypt.js
+++ scrypt.js
@@ -1,42 +1,41 @@
+let textArea = document.querySelector("#decoder-text-input");
+let hideImage = document.querySelector(".out-text-undefined");
+let showText = document.querySelector(".out-text-defined");
+let outText = document.querySelector(".out-text-decoder");
 
-let textArea = document.querySelector('#decoder-text-input');
-let hideImage = document.querySelector('.out-text-undefined');
-let showText = document.querySelector('.out-text-defined');
-
-let outText = document.querySelector('.out-text-decoder');
-
-
-function hideElement (){
-    if (textArea.value == ''){
-        hideImage.style.display='';
-        showText.style.display='none';
-    } else {
-        hideImage.style.display='none';
-        showText.style.display='';
-    }
+function hideElement() {
+  if (textArea.value == "") {
+    hideImage.style.display = "";
+    showText.style.display = "none";
+  } else {
+    hideImage.style.display = "none";
+    showText.style.display = "";
+  }
 }
 
-function encryptText(){
-    hideElement();
-    let text = textArea.value;
-    text = text.replaceAll("e", "enter");
-    text = text.replaceAll("i", "imes");
-    text = text.replaceAll("a", "ai");
-    text = text.replaceAll("o", "ober");
-    text = text.replaceAll("u", "ufat");
-    console.log(text);
-    outText.innerHTML = text;
-
+function encryptText() {
+  hideElement();
+  let text = textArea.value;
+  text = text.replaceAll("e", "enter");
+  text = text.replaceAll("i", "imes");
+  text = text.replaceAll("a", "ai");
+  text = text.replaceAll("o", "ober");
+  text = text.replaceAll("u", "ufat");
+  outText.innerHTML = text;
 }
 
-function decoderText(){
-    hideElement();
-    let text = textArea.value;
-    text = text.replaceAll("enter", "e");
-    text = text.replaceAll("imes", "i");
-    text = text.replaceAll("ai", "a");
-    text = text.replaceAll("ober", "o");
-    text = text.replaceAll("ufat", "u");
-    outText.innerHTML = text;
+function decoderText() {
+  hideElement();
+  let text = textArea.value;
+  text = text.replaceAll("enter", "e");
+  text = text.replaceAll("imes", "i");
+  text = text.replaceAll("ai", "a");
+  text = text.replaceAll("ober", "o");
+  text = text.replaceAll("ufat", "u");
+  outText.innerHTML = text;
 }
 
+function copyText() {
+  let copy = document.querySelector('.out-text-decoder').innerHTML;
+  navigator.clipboard.writeText(copy);
+}
