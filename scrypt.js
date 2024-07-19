8b5adc031e041623619b2844b4443737036edfa4
Leonan Cardoso <leonangomes1311@hotmail.com>
2024-07-18 23:57:50 -0300

    Adicionado a funcao de decodificar

--- scrypt.js
+++ scrypt.js
@@ -3,6 +3,8 @@ let textArea = document.querySelector('#decoder-text-input');
 let hideImage = document.querySelector('.out-text-undefined');
 let showText = document.querySelector('.out-text-defined');
 
+let outText = document.querySelector('.out-text-decoder');
+
 
 function hideElement (){
     if (textArea.value == ''){
@@ -17,16 +19,24 @@ function hideElement (){
 function encryptText(){
     hideElement();
     let text = textArea.value;
-
     text = text.replaceAll("e", "enter");
     text = text.replaceAll("i", "imes");
     text = text.replaceAll("a", "ai");
     text = text.replaceAll("o", "ober");
     text = text.replaceAll("u", "ufat");
     console.log(text);
+    outText.innerHTML = text;
+
 }
 
 function decoderText(){
     hideElement();
+    let text = textArea.value;
+    text = text.replaceAll("enter", "e");
+    text = text.replaceAll("imes", "i");
+    text = text.replaceAll("ai", "a");
+    text = text.replaceAll("ober", "o");
+    text = text.replaceAll("ufat", "u");
+    outText.innerHTML = text;
 }
 
