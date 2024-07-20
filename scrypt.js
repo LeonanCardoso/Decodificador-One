66b73ec849c23074f3106a245e17111ad1a2c548
Leonan Cardoso <leonangomes1311@hotmail.com>
2024-07-20 10:49:45 -0300

    Adicionado verificacao de texto e algumas melhorias no visual de erro

--- scrypt.js
+++ scrypt.js
@@ -4,7 +4,7 @@ let showText = document.querySelector(".out-text-defined");
 let outText = document.querySelector(".out-text-decoder");
 
 function hideElement() {
-  if (textArea.value == "") {
+  if (textArea.value == "" && verifyText(text)) {
     hideImage.style.display = "";
     showText.style.display = "none";
   } else {
@@ -13,29 +13,68 @@ function hideElement() {
   }
 }
 
+function showPopoutError() {
+  var popout = document.querySelector(".popout");
+  var buttons = document.querySelectorAll(".buttons-section .btn");
+  popout.style.display = "block";
+  
+  buttons.forEach(function(button) {
+    button.style.background = "red";
+    button.style.color = "white";
+    button.style.border = "none";
+  });
+  setTimeout(function () {
+    popout.style.display = "none";
+    buttons.forEach(function(button) {
+      button.style.background = "";
+      button.style.color = "";
+    button.style.border = "";
+    });
+  }, 3000);
+}
+
+function showElementError() {
+  hideImage.style.display = "";
+  showText.style.display = "none";
+}
+
+function verifyText(text) {
+  return !/[A-ZÀ-ü0-9]/.test(text);
+}
+
 function encryptText() {
-  hideElement();
   let text = textArea.value;
-  text = text.replaceAll("e", "enter");
-  text = text.replaceAll("i", "imes");
-  text = text.replaceAll("a", "ai");
-  text = text.replaceAll("o", "ober");
-  text = text.replaceAll("u", "ufat");
-  outText.innerHTML = text;
+  if (verifyText(text)) {
+    text = text.replaceAll("e", "enter");
+    text = text.replaceAll("i", "imes");
+    text = text.replaceAll("a", "ai");
+    text = text.replaceAll("o", "ober");
+    text = text.replaceAll("u", "ufat");
+    outText.innerHTML = text;
+    hideElement();
+  } else {
+    showElementError();
+    showPopoutError();
+  }
 }
 
 function decoderText() {
-  hideElement();
   let text = textArea.value;
-  text = text.replaceAll("enter", "e");
-  text = text.replaceAll("imes", "i");
-  text = text.replaceAll("ai", "a");
-  text = text.replaceAll("ober", "o");
-  text = text.replaceAll("ufat", "u");
-  outText.innerHTML = text;
+  if (verifyText(text) && text != "") {
+    hideElement();
+    text = text.replaceAll("enter", "e");
+    text = text.replaceAll("imes", "i");
+    text = text.replaceAll("ai", "a");
+    text = text.replaceAll("ober", "o");
+    text = text.replaceAll("ufat", "u");
+    outText.innerHTML = text;
+  } else {
+    showElementError();
+    showPopoutError();
+  }
 }
 
 function copyText() {
-  let copy = document.querySelector('.out-text-decoder').innerHTML;
+  let copy = document.querySelector(".out-text-decoder").innerHTML;
   navigator.clipboard.writeText(copy);
 }
